9ec0f036d04476f61cc28ecdd914f4c70d88252f
Zak <zak.hajjaji@icloud.com>
2025-07-14 09:36:01 +0100

    condition created for secret number generator

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,22 +1,31 @@
-import java.util.Scanner; //scanner lets me read user input
-import java.util.Random; //lets me generate random numbers like math.random etc
+import java.util.Scanner; //scanner lets me read user input, standard tool in java 
+import java.util.Random; //lets me generate random numbers like math.random etc, again, another tool 
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner scanner = new Scanner(System.in); // system.in is an input stream(keyboard)
+        Random random = new Random(); // random number generator
 
-        int secretNumber = random.nextInt(100) + 1;
-        int guess = 0;
-        int attempts = 0;
+        int secretNumber = random.nextInt(100) + 1; // random number from 0-99 BUT the +1 allows to 100. efficient
+                                                    // memory allocations
+        int guess = 0; // default value against secret number
+        int attempts = 0; // simple counter
 
         System.out.println("Welcome to the number guessing game!");
         System.out.println("I am thinking of a number between 1 and 100 ... ");
 
-        // while statement next
-        // system print, enter your guess and then add attempt ++
-        // if statement after, too low or too high ... else statement to congratulate
-        // them with + attempts + tries
-        // scanner. close !!
+        while (guess != secretNumber) {
+            System.out.println("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+            if (guess < secretNumber) {
+                System.out.println("Too low! ");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! ");
+            } else {
+                System.out.println("Correct !! It took you " + attempts + " tries");
+            }
+        }
+        scanner.close();
     }
 }

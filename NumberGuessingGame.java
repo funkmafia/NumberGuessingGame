b97580f6ff6bff9f94b5f375247fce7e75752fb4
Zak <zak.hajjaji@icloud.com>
2025-07-14 10:11:59 +0100

    play again method done

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,23 +1,34 @@
-import java.util.Scanner; //scanner lets me read user input, standard tool in java 
-import java.util.Random; //lets me generate random numbers like math.random etc, again, another tool 
+import java.util.Scanner; // IMPORT STATEMENT 
+import java.util.Random;
 
-public class NumberGuessingGame {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // system.in is an input stream(keyboard)
-        Random random = new Random(); // random number generator
+public class NumberGuessingGame { // CLASS DECLARATION
+    public static void main(String[] args) { // METHOD DECLARATION
+        Scanner scanner = new Scanner(System.in); // OBJECT INSTANTIATION
+        playGame(scanner); // METHOD CALL
+        String playAgain; // VARIABLE DECLARATION
+        do {
+            System.out.println("Play again? Yes or No");
+            playAgain = scanner.next().toLowerCase(); // METHOD CHAIN
+            if (playAgain.equals("Yes")) {
+                playGame(scanner);
+            }
+        } while (playAgain.equals("Yes"));
+        scanner.close();
+    }
 
-        int secretNumber = random.nextInt(100) + 1; // random number from 0-99 BUT the +1 allows to 100. efficient
-                                                    // memory allocations
-        int guess = 0; // default value against secret number
-        int attempts = 0; // simple counter
+    public static void playGame(Scanner scanner) { // METHOD DECLARATION
+        Random random = new Random(); // OBJECT INSTANTIATION
+        int secretNumber = random.nextInt(100) + 1; // VARAIBLE DECLARATION
+        int guess = 0; // VARAIBLE DECLARATION
+        int attempts = 0; // VARAIBLE DECLARATION
 
         System.out.println("Welcome to the number guessing game!");
         System.out.println("I am thinking of a number between 1 and 100 ... ");
 
         while (guess != secretNumber) {
             System.out.println("Enter your guess: ");
-            guess = scanner.nextInt();
-            attempts++;
+            guess = scanner.nextInt(); // VARIABLE ASSIGNMENT WITH METHOD CALL
+            attempts++; // INCREMENT STATEMENT
             if (guess < secretNumber) {
                 System.out.println("Too low! ");
             } else if (guess > secretNumber) {
@@ -27,5 +38,5 @@ public class NumberGuessingGame {
             }
         }
         scanner.close();
-    }
-}
+    } // END OF PLAYGAME() METHOD
+} // END OF CLASS
